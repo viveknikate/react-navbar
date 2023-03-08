6f3548fd59783cb3b963498d9ec4e2e82ee4862c
Vivek <viveknikate.2000@gmail.com>
2023-03-08 10:23:30 +0530

    react-navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React from 'react';
+import './styles/Header.css';
+import './styles/App.css';
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from './Components/Header';       
+import HomePage from './Components/Home';
+import BlogPage from './Components/Blog';
+import About from './Components/About';
+import ContactPage from './Components/Contact';
+import ProductName from './Components/Product'
+import ArticlePage from './Components/Article';
+
+export default function App() {
+  return(
+  <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/CONTACT' element={<ContactPage />} />
+      <Route path='/AbOuT' element={<About />} />
+      <Route path='/Blog' element={<BlogPage />} />
+      <Route path='/product/:id' element={<ProductName />} />
+      <Route path='/article/:ArtNum' element={<ArticlePage />} />
+      <Route path='*' element={<h1 style={{color:'red'}} >Page not Found 404</h1>} />
+    </Routes>
+  </Router>
   );
 }
-
-export default App;
---
+++ src/Components/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () =>{
+    return(
+        <React.Fragment>
+            <h1>Blog Page</h1>
+            <p>Here new blog goes</p>
+        </React.Fragment>
+    );
+}
+
+export default Blog;
